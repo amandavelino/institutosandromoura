399dc12b3a42f271b4627091b38d9ee37bab046c
Amanda Avelino <amavlopes@gmail.com>
2017-07-19 13:31:28 -0300

    Desenvolvimento sessão #depoimentos - não responsivo.

---
+++ incs/depoimentos.php
@@ -0,0 +1,44 @@
+<section id="depoimentos">
+
+    <div class="container">
+
+        <div class="owl-carousel owl-theme owl-depoimentos">
+
+            <div class="item">
+                <div class="item-midia">
+                    <figure>
+                        <img class="img-responsive" src="img/depoimento-slide01.jpg" alt="Marcos dos Santos - Rio de Janeiro" border="0">
+                    </figure>
+                </div>
+                <!-- end .midia -->
+                <div class="item-conteudo">
+                    <h2>Marcos dos Santos - Rio de Janeiro</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. Proin sodales pulvinar tempor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+                    <p>Nam fermentum, nulla luctus pharetra vulputate, felis tellus mollis orci, sed rhoncus sapien nunc eget odio. Nam fermentum, nulla luctus pharetra vulputate.</p>
+                    <p>Proin sodales pulvinar tempor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+                </div> 
+                <!-- end .item-conteudo -->
+            </div><!-- end .item -->
+
+            <div class="item">
+                <div class="item-midia">
+                    <figure>
+                        <img class="img-responsive" src="img/depoimento-slide01.jpg" alt="Marcos dos Santos - Rio de Janeiro" border="0">
+                    </figure>
+                </div>
+                <!-- end .midia -->
+                <div class="item-conteudo">
+                    <h2>Paulo Santana - São Paulo</h2>
+                    <p>Proin sodales pulvinar tempor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+                    <p>Nam fermentum, nulla luctus pharetra vulputate, felis tellus mollis orci, sed rhoncus sapien nunc eget odio. Nam fermentum, nulla luctus pharetra vulputate.</p>
+                </div>
+                <!-- end .item-conteudo -->
+            </div><!-- end .item -->
+
+        </div><!-- end .owl-carousel -->
+
+    </div>
+    <!-- end .container -->
+
+</section>
+<!-- end #depoimentos -->
--- index.php
+++ index.php
@@ -5,9 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Instituto Sandro Moura</title>
-    <script src="https://use.typekit.net/zhd0tfh.js"></script>
-    <script>try{Typekit.load({ async: true });}catch(e){}</script>
     <link rel="stylesheet" href="css/style.css">
+    <script src="https://use.typekit.net/zhd0tfh.js"></script>
+    <script>try{Typekit.load({ async: true });}catch(e){}</script>    
 </head>
 <body>
 
@@ -15,6 +15,31 @@
     <?php include("./incs/metodologia.php");?>
     <?php include("./incs/aulas.php");?>
     <?php include("./incs/assinatura.php");?>
+    <?php include("./incs/depoimentos.php");?>
+
+    <script src="./bower_components/jquery/dist/jquery.min.js"></script>
+    <script src="./bower_components/owl.carousel/dist/owl.carousel.min.js"></script>
+    <script>
+
+        $(document).ready(function(){
+            //Slider Depoimentos
+            $('.owl-depoimentos').owlCarousel({
+                animateIn: 'fadeIn',
+                animateOut: 'fadeOut',
+                mouseDrag: false,
+                smartSpeed: 450,
+                margin: 20,
+                loop: true,
+                margin: 0,
+                dots: true,
+                items: 1,
+                autoplay: true,
+                autoplayTimeout: 5000,
+                autoplayHoverPause: true
+            });
+        });
+
+    </script>
 
 </body>
 </html>
