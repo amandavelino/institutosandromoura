bd66f7865c09e715bd5d4e91b6561e108cadbd33
Amanda Avelino <amavlopes@gmail.com>
2017-07-19 13:56:07 -0300

    Desenvolvimento sessão #parceiros - não responsivo.

---
+++ incs/parceiros.php
@@ -0,0 +1,28 @@
+<section id="parceiros">
+
+    <div class="container">
+        
+        <header>
+            <h2>Nossos Parceiros</h2>
+        </header>
+
+        <ul class="lista-parceiros">
+            <li><img src="./img/icon-parceiros-tmp.jpg" alt="DW Drums" border="0"></li>
+            <li><img src="./img/icon-parceiros-tmp.jpg" alt="DW Drums" border="0"></li>
+            <li><img src="./img/icon-parceiros-tmp.jpg" alt="DW Drums" border="0"></li>
+            <li><img src="./img/icon-parceiros-tmp.jpg" alt="DW Drums" border="0"></li>
+            <li><img src="./img/icon-parceiros-tmp.jpg" alt="DW Drums" border="0"></li>
+            <li><img src="./img/icon-parceiros-tmp.jpg" alt="DW Drums" border="0"></li>
+            <li><img src="./img/icon-parceiros-tmp.jpg" alt="DW Drums" border="0"></li>
+            <li><img src="./img/icon-parceiros-tmp.jpg" alt="DW Drums" border="0"></li>
+            <li><img src="./img/icon-parceiros-tmp.jpg" alt="DW Drums" border="0"></li>
+            <li><img src="./img/icon-parceiros-tmp.jpg" alt="DW Drums" border="0"></li>
+            <li><img src="./img/icon-parceiros-tmp.jpg" alt="DW Drums" border="0"></li>
+            <li><img src="./img/icon-parceiros-tmp.jpg" alt="DW Drums" border="0"></li>
+        </ul> 
+
+    </div>
+    <!-- end .container -->
+
+</section>
+<!-- end #parceiros -->
--- index.php
+++ index.php
@@ -16,6 +16,7 @@
     <?php include("./incs/aulas.php");?>
     <?php include("./incs/assinatura.php");?>
     <?php include("./incs/depoimentos.php");?>
+    <?php include("./incs/parceiros.php");?>
 
     <script src="./bower_components/jquery/dist/jquery.min.js"></script>
     <script src="./bower_components/owl.carousel/dist/owl.carousel.min.js"></script>
