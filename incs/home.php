b9335eedd2d2f90731a055a596eefde80290bb56
Amanda Avelino <amavlopes@gmail.com>
2017-07-31 13:03:09 -0300

    Arquivos CSS com sourcemap. Finalizado sessão #home responsivo.

--- incs/home.php
+++ incs/home.php
@@ -6,8 +6,8 @@
 
             <h1 id="logo">
                 <a href="#" title="Instituto Sandro Moura">
-                    <svg width="334" height="90">
-                        <image xmlns:xlink="./img/ism.svg" xlink:href="./img/ism.svg" src="./img/ism.png" width="334" height="90"></image>
+                    <svg viewBox="0 0 293 80" preserveAspectRatio="xMidYMid meet">
+                        <image xmlns:xlink="./img/ism.svg" xlink:href="./img/ism.svg" src="./img/ism.png"></image>
                     </svg> 
                 </a>                  
             </h1><!-- logo -->
@@ -20,16 +20,22 @@
             <form id="inscricao" class="inscricao">
 
                     <fieldset>
-                        <h3>Inicie já seu <strong>mês gratuíto</strong></h3>
-                        <p>Garantimos 100% de satisfação. Você só passa a pagar no próximo mês. E se quiser pode cancelar sem nenhum custo!</p>
-                        <label for="nome">
-                            <p>Nome</p>
-                            <input type="text" id="nome" name="nome">
-                        </label>
-                        <label for="email">
-                            <p>Email</p>
-                            <input type="text" id="email" name="email">
-                        </label> 
+                        <div class="box-form">
+                            <h3>Inicie já seu <strong>mês gratuíto</strong></h3>
+                            <p>Garantimos 100% de satisfação. Você só passa a pagar no próximo mês. E se quiser pode cancelar sem nenhum custo!</p>
+                        </div>
+                        <!-- end .box-form -->
+                        <div class="box-form">
+                            <label for="nome">
+                                <p>Nome</p>
+                                <input type="text" id="nome" name="nome">
+                            </label>
+                            <label for="email">
+                                <p>Email</p>
+                                <input type="text" id="email" name="email">
+                            </label> 
+                        </div>
+                        <!-- end .box-form -->                        
                         <input class="btn-enviar" type="submit" name="inscricao" value="Iniciar!">
                     </fieldset>
 
