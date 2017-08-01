326d2df2fa8b89edb63b25a769362a58aa4db09f
Amanda Avelino <amavlopes@gmail.com>
2017-08-01 13:24:17 -0300

    Sessão #aulas responsiva finalizada.

--- incs/aulas.php
+++ incs/aulas.php
@@ -10,17 +10,23 @@
             <figure>
                 <img src="./img/facilidade-sistema.jpg" alt="Facilidade no uso do sistema" border="0">
             </figure>
-            <h3>Facilidade no uso do sistema</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean adpai ei</p>
+            <div class="txt-artigo">
+                <h3>Facilidade no uso do sistema</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean adpai ei</p>                
+            </div>
+            <!-- end .txt-artigo -->
         </article>
         <!-- end .item-aula -->
 
-        <article class="item-aula">
+        <article class="item-aula md">
             <figure>
                 <img src="./img/assista-online.jpg" alt="Assista online onde você estiver" border="0">
             </figure>
-            <h3>Assista online onde você estiver</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean adpai ei</p>
+            <div class="txt-artigo">
+                <h3>Assista online onde você estiver</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean adpai ei</p>                
+            </div>
+            <!-- end .txt-artigo -->            
         </article>
         <!-- end .item-aula -->
         
@@ -28,8 +34,11 @@
             <figure>
                 <img src="./img/entre-aproveite.jpg" alt="É só entrar e aproveitar!" border="0">
             </figure>
-            <h3>É só entrar e aproveitar!</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean adpai ei</p>
+            <div class="txt-artigo">
+                <h3>É só entrar e aproveitar!</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean adpai ei</p>                
+            </div>
+            <!-- end .txt-artigo -->            
         </article>
         <!-- end .item-aula -->        
 
